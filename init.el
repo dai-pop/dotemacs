e3358afc59797f467a615baba9ec0c8fe5083f09
dai-pop <114280166+dai-pop@users.noreply.github.com>
2022-11-24 14:36:58 +0900

    init

---
+++ init.el
@@ -0,0 +1,448 @@
+;;; init.el --- My init.el  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Naoya Yamashita
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; My init.el.
+
+;;; Code:
+
+;; this enables this running method
+;;   emacs -q -l ~/.debug.emacs.d/init.el
+(eval-and-compile
+  (when (or load-file-name byte-compile-current-file)
+    (setq user-emacs-directory
+          (expand-file-name
+           (file-name-directory (or load-file-name byte-compile-current-file))))))
+
+(eval-and-compile
+  (customize-set-variable
+   'package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
+                       ("melpa" . "https://melpa.org/packages/")
+                       ("org"   . "https://orgmode.org/elpa/")))
+  (package-initialize)
+  (unless (package-installed-p 'leaf)
+    (package-refresh-contents)
+    (package-install 'leaf))
+
+  (leaf leaf-keywords
+    :ensure t
+    :init
+    ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
+    (leaf hydra :ensure t)
+    (leaf el-get :ensure t)
+    (leaf blackout :ensure t)
+
+    :config
+    ;; initialize leaf-keywords.el
+    (leaf-keywords-init)))
+
+;; ここにいっぱい設定を書く
+
+(leaf leaf
+  :config
+  (leaf leaf-convert :ensure t)
+  (leaf leaf-tree
+    :ensure t
+    :custom ((imenu-list-size . 30)
+             (imenu-list-position . 'left))))
+
+(leaf macrostep
+  :ensure t
+  :bind (("C-c e" . macrostep-expand)))
+
+(leaf cus-edit
+  :doc "tools for customizing Emacs and Lisp packages"
+  :tag "builtin" "faces" "help"
+  :custom `((custom-file . ,(locate-user-emacs-file "custom.el"))))
+
+
+(leaf cus-start
+  :doc "define customization properties of builtins"
+  :tag "builtin" "internal"
+  :preface
+  (defun c/redraw-frame nil
+    (interactive)
+    (redraw-frame))
+
+  :bind (("M-ESC ESC" . c/redraw-frame))
+  :custom '((user-full-name . "Daisuke Nakamura")
+            (user-mail-address . "dn@example.com")
+            (user-login-name . "nakamud")
+            (create-lockfiles . nil)
+            (debug-on-error . t)
+            (init-file-debug . t)
+            (frame-resize-pixelwise . t)
+;            (enable-recursive-minibuffers . t)
+            (history-length . 1000)
+            (history-delete-duplicates . t)
+            (scroll-preserve-screen-position . t)
+            (scroll-conservatively . 100)
+            (mouse-wheel-scroll-amount . '(1 ((control) . 5)))
+            (ring-bell-function . 'ignore)
+            (text-quoting-style . 'straight)
+            (truncate-lines . t)
+            ;; (use-dialog-box . nil)
+            ;; (use-file-dialog . nil)
+            ;; (menu-bar-mode . t)
+            (tool-bar-mode . nil)
+            (indent-tabs-mode . nil)
+            (blink-cursor-mode . nil)
+            (column-number-mode . t)
+            (size-indication-mode . t)
+            )
+  :config
+  (defalias 'yes-or-no-p 'y-or-n-p)
+  (keyboard-translate ?\C-h ?\C-?))
+
+
+(leaf general-setting
+  :config
+  (prefer-coding-system 'utf-8-unix)
+  (defalias 'yes-or-no-p 'y-or-n-p) ; yes-or-no-pをy/nで選択できるようにする
+  (defvar recentf-max-saved-items 1000)
+  (defvar recentf-auto-cleanup 'never)
+  (show-paren-mode t)
+  (defvar show-paren-style 'mixed)
+  ;; カーソルを点滅させない
+  (blink-cursor-mode 0)
+
+  :setq
+  `((large-file-warning-threshold	         . ,(* 25 1024 1024))
+    (read-file-name-completion-ignore-case . t)
+    (use-dialog-box                        . nil)
+    (history-length                        . 500)
+    (history-delete-duplicates             . t)
+    (line-move-visual                      . nil)
+    (mouse-drag-copy-region                . t)
+    (backup-inhibited                      . t)
+    (inhibit-startup-message               . t)
+    (require-final-newline                 . t)
+    (next-line-add-newlines                . nil)
+    (truncate-lines                        . t)
+    (read-process-output-max               . ,(* 1024 1024)))
+
+  :setq-default
+  (indent-tabs-mode . nil) ; タブはスペースで
+  (tab-width        . 2)
+  (require-final-newline . t)
+  )
+
+
+(leaf autorevert
+  :doc "revert buffers when files on disk change"
+  :tag "builtin"
+  :custom ((auto-revert-interval . 1))
+  :global-minor-mode global-auto-revert-mode)
+
+
+
+
+(leaf paren
+  :doc "highlight matching paren"
+  :tag "builtin"
+  :custom ((show-paren-delay . 0.1))
+  :global-minor-mode show-paren-mode)
+
+
+
+
+(leaf simple
+  :doc "basic editing commands for Emacs"
+  :tag "builtin" "internal"
+  :custom ((kill-ring-max . 100)
+           (kill-read-only-ok . t)
+           (kill-whole-line . t)
+           (eval-expression-print-length . nil)
+           (eval-expression-print-level . nil)))
+
+
+
+(leaf files
+  :doc "file input and output commands for Emacs"
+  :tag "builtin"
+  :custom `((auto-save-timeout . 15)
+            (auto-save-interval . 60)
+            (auto-save-file-name-transforms . '((".*" ,(locate-user-emacs-file "backup/") t)))
+            (backup-directory-alist . '((".*" . ,(locate-user-emacs-file "backup"))
+                                        (,tramp-file-name-regexp . nil)))
+            (version-control . t)
+            (delete-old-versions . t)))
+
+
+
+
+
+
+(leaf startup
+  :doc "process Emacs shell arguments"
+  :tag "builtin" "internal"
+  :custom `((auto-save-list-file-prefix . ,(locate-user-emacs-file "backup/.saves-"))))
+
+
+(leaf ivy
+  :doc "Incremental Vertical completYon"
+  :req "emacs-24.5"
+  :tag "matching" "emacs>=24.5"
+  :url "https://github.com/abo-abo/swiper"
+  :emacs>= 24.5
+  :ensure t
+  :blackout t
+  :leaf-defer nil
+  :custom ((ivy-initial-inputs-alist . nil)
+           (ivy-use-selectable-prompt . t))
+  :global-minor-mode t
+  :config
+  (leaf swiper
+    :doc "Isearch with an overview. Oh, man!"
+    :req "emacs-24.5" "ivy-0.13.0"
+    :tag "matching" "emacs>=24.5"
+    :url "https://github.com/abo-abo/swiper"
+    :emacs>= 24.5
+    :ensure t
+    :bind (("C-s" . swiper)))
+
+  (leaf counsel
+    :doc "Various completion functions using Ivy"
+    :req "emacs-24.5" "swiper-0.13.0"
+    :tag "tools" "matching" "convenience" "emacs>=24.5"
+    :url "https://github.com/abo-abo/swiper"
+    :emacs>= 24.5
+    :ensure t
+    :blackout t
+    :bind (("C-S-s" . counsel-imenu)
+           ("C-x C-v" . counsel-recentf))
+    :custom `((counsel-yank-pop-separator . "\n----------\n")
+              (counsel-find-file-ignore-regexp . ,(rx-to-string '(or "./" "../") 'no-group)))
+    :global-minor-mode t))
+
+(leaf prescient
+  :doc "Better sorting and filtering"
+  :req "emacs-25.1"
+  :tag "extensions" "emacs>=25.1"
+  :url "https://github.com/raxod502/prescient.el"
+  :emacs>= 25.1
+  :ensure t
+  :custom ((prescient-aggressive-file-save . t))
+  :global-minor-mode prescient-persist-mode)
+  
+(leaf ivy-prescient
+  :doc "prescient.el + Ivy"
+  :req "emacs-25.1" "prescient-4.0" "ivy-0.11.0"
+  :tag "extensions" "emacs>=25.1"
+  :url "https://github.com/raxod502/prescient.el"
+  :emacs>= 25.1
+  :ensure t
+  :after prescient ivy
+  :custom ((ivy-prescient-retain-classic-highlighting . t))
+  :global-minor-mode t)
+
+
+
+
+
+
+(leaf flycheck
+  :doc "On-the-fly syntax checking"
+  :req "dash-2.12.1" "pkg-info-0.4" "let-alist-1.0.4" "seq-1.11" "emacs-24.3"
+  :tag "minor-mode" "tools" "languages" "convenience" "emacs>=24.3"
+  :url "http://www.flycheck.org"
+  :emacs>= 24.3
+  :ensure t
+  :bind (("M-n" . flycheck-next-error)
+         ("M-p" . flycheck-previous-error))
+  :global-minor-mode global-flycheck-mode)
+
+
+
+
+
+(leaf company
+  :doc "Modular text completion framework"
+  :req "emacs-24.3"
+  :tag "matching" "convenience" "abbrev" "emacs>=24.3"
+  :url "http://company-mode.github.io/"
+  :emacs>= 24.3
+  :ensure t
+  :blackout t
+  :leaf-defer nil
+  :bind ((company-active-map
+          ("M-n" . nil)
+          ("M-p" . nil)
+          ("C-s" . company-filter-candidates)
+          ("C-n" . company-select-next)
+          ("C-p" . company-select-previous)
+          ("<tab>" . company-complete-selection))
+         (company-search-map
+          ("C-n" . company-select-next)
+          ("C-p" . company-select-previous)))
+  :custom ((company-idle-delay . 0)
+           (company-minimum-prefix-length . 1)
+           (company-transformers . '(company-sort-by-occurrence)))
+  :global-minor-mode global-company-mode)
+
+(leaf company-c-headers
+  :doc "Company mode backend for C/C++ header files"
+  :req "emacs-24.1" "company-0.8"
+  :tag "company" "development" "emacs>=24.1"
+  :added "2020-03-25"
+  :emacs>= 24.1
+  :ensure t
+  :after company
+  :defvar company-backends
+  :config
+  (add-to-list 'company-backends 'company-c-headers))
+
+
+
+(leaf dumb-jump
+  :if (executable-find "rg")
+  :ensure t
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-." . dumb-jump-go)
+         ("M-*" . dumb-jump-back)
+         ("M-," . dumb-jump-quick-look)
+         ("M-g i" . dumb-jump-go-prompt)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :custom '((dumb-jump-selector       . 'ivy)
+            (dumb-jump-force-searcher . 'rg))
+  )
+
+
+(leaf ripgrep
+  :ensure t
+  :bind (("C-c r" . ripgrep-regexp)
+         )
+  )
+
+
+(provide 'init)
+
+
+
+(defconst my:saved-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq file-name-handler-alist my:saved-file-name-handler-alist)))
+
+(leaf font
+  :config
+  ;; 絵文字
+  ;;(unicode-fonts-setup) ; 最初に本コマンドの実行が必要
+  ;;(all-the-icons-install-fonts)
+  (leaf unicode-fonts
+    :ensure t)
+  (leaf all-the-icons
+    :ensure t)
+  ;; フォント設定
+  ;;
+  ;; abcdefghik
+  ;; 0123456789
+  ;; あいうえお
+  (let* ((family "Cica")
+         (fontspec (font-spec :family family :weight 'normal)))
+    (set-face-attribute 'default nil :family family :height 120)
+    (set-fontset-font nil 'ascii fontspec nil 'append)
+    (set-fontset-font nil 'japanese-jisx0208 fontspec nil 'append))
+  (add-to-list 'face-font-rescale-alist '(".*icons.*" . 0.9))
+  (add-to-list 'face-font-rescale-alist '(".*FontAwesome.*" . 0.9))
+  (leaf text-scale
+    :hydra (hydra-zoom ()
+                       "Zoom"
+                       ("g" text-scale-increase "in")
+                       ("l" text-scale-decrease "out")
+                       ("r" (text-scale-set 0) "reset")
+                       ("0" (text-scale-set 0) :bind nil :exit t))
+    :bind ("<f2>" . hydra-zoom/body)))
+
+
+
+
+
+(global-set-key (kbd "C-h")       'backward-delete-char)
+(global-set-key (kbd "C-m")       'newline-and-indent)
+(global-set-key (kbd "C-/")       'undo)
+(global-set-key (kbd "C-?") 'redo)
+(global-set-key (kbd "C-z")       'scroll-down)
+(global-set-key (kbd "C-x C-b")   'ibuffer)
+(global-set-key (kbd "C-x C-m")   'compile)
+
+;(require 'mura-util)
+(global-unset-key "\C-q")
+(global-set-key "\C-q\C-q" 'quoted-insert)
+(global-set-key "\C-q\C-e" 'my-open-explorer)
+(global-set-key "\C-q\C-s" 'my-xyzzy-grep)
+(global-set-key "\C-q\C-j" 'open-junk-file)
+
+(global-set-key (kbd "C-;") 'other-window)
+(global-set-key (kbd "C-o") 'call-last-kbd-macro)
+
+
+(global-set-key  [C-tab] 'next-multiframe-window)
+(global-set-key  [C-S-tab] 'previous-multiframe-window)
+
+(global-set-key "\C-xt" 'ff-find-other-file)
+
+;; meta key
+(setq ns-command-modifier (quote meta))
+(setq ns-alternate-modifier (quote super))
+
+;; バックアップファイルを残さない
+(setq make-backup-files nil)
+
+;; ベルを鳴らさない
+(setq ring-bell-function 'ignore)
+
+(setq kill-whole-line t)
+
+;; 1行ずつスクロール
+(setq scroll-step 1)
+
+;; ステータスに行番号＆列番号表示
+(column-number-mode t)
+
+;; 折り返さない
+(setq truncate-lines t)
+(setq truncate-partial-width-windows t)
+
+;; インデント設定
+(setq-default indent-tabs-mode nil)
+(setq-default c-basic-offset 4)
+(setq-default tab-width 4)
+(c-set-offset 'case-label '+)
+
+;; narrowingを使う
+(put 'narrow-to-region 'disabled nil)
+
+;; スタートページ非表示
+(setq inhibit-startup-message t)
+
+;; タイムローケールを英語に
+(setq system-time-locale "C")
+
+(setq read-file-name-completion-ignore-case t)
+
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+
+;;; init.el ends here
