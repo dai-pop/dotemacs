a0328ea6d8e7c967d0155cb4040d837d2362a668
dai-pop <114280166+dai-pop@users.noreply.github.com>
2022-11-25 22:52:55 +0900

    custom.elがなぜか読まれないのでこっちで

--- init.el
+++ init.el
@@ -55,6 +55,14 @@
 
 ;; ここにいっぱい設定を書く
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 143 :width normal)))))
+
+
 (leaf leaf
   :config
   (leaf leaf-convert :ensure t)
